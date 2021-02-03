7ccbd88100f019e0d5c9f0872870675feb7c98c3
Rob Sterner <fermion@github.com>
2021-02-03 09:23:26 -0500

    attempt to purge

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: [
     require('@tailwindcss/ui'),
-  ]
+  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      '../../views/**/*.html.erb',
+      '../packs/**/*.js',
+      '../controllers/**/*.js',
+    ]
+  }
 }
