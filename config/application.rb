44a13c707dba058ca8e7aaa3b1b5c85d520b0383
Rob Sterner <fermion@github.com>
2021-02-04 21:54:00 -0500

    set Access-Control-Allow-Credentials to true

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,10 @@ module HydrantsTracker
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins /boxfordfire.org|thehydrantapp.com/
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :options],
+          credentials: true
       end
     end
   end
