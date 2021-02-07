7d83509b719c352db67812921225778dd3860af8
Rob Sterner <fermion@github.com>
2021-02-07 15:48:59 -0500

    for now don't purge anything. let's see how heavy the CSS is

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
   plugins: [
     require('@tailwindcss/ui'),
   ],
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      '../../views/**/*.html.erb',
-      '../packs/**/*.js',
-      '../controllers/**/*.js',
-    ]
-  }
+  // purge: {
+  //   enabled: process.env.NODE_ENV === 'production',
+  //   content: [
+  //     '../../views/**/*.html.erb',
+  //     '../packs/**/*.js',
+  //     '../controllers/**/*.js',
+  //   ]
+  // }
 }
