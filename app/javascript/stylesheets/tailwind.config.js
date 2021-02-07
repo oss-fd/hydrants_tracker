b72ce4d9e69e2adfab7aea5e5c7f0bc482e55747
Rob Sterner <fermion@github.com>
2021-02-07 16:14:15 -0500

    properly point postcss purging at the views directory

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -2,12 +2,10 @@ module.exports = {
   plugins: [
     require('@tailwindcss/ui'),
   ],
-  // purge: {
-  //   enabled: process.env.NODE_ENV === 'production',
-  //   content: [
-  //     '../../views/**/*.html.erb',
-  //     '../packs/**/*.js',
-  //     '../controllers/**/*.js',
-  //   ]
-  // }
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './app/**/*.html.erb',
+    ]
+  }
 }
